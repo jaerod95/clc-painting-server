1b22e5b44ea5ee1dceb53980f9bebbd361b73162
Jason Rodriguez <jason@frenteventures.com>
2018-10-10 15:21:40 -0600

    Added base user Model

---
+++ gantt/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg id="gantt">
+  </svg>
+</template>
+
+<script>
+import Gantt from 'frappe-gantt';
+
+export default {
+  name: 'home',
+  mounted() {
+    var tasks = [
+      {
+        id: 'Task 1',
+        name: 'Redesign website',
+        start: '2016-12-28',
+        end: '2016-12-31',
+        progress: 20,
+        dependencies: 'Task 2, Task 3',
+        aaaa: 'Jason',
+      },
+    ];
+    var gantt = new Gantt(document.getElementById('gantt'), tasks, {
+      custom_popup_html: task => {
+        console.log(task);
+        return `
+        <div class="details-container">
+          <p>${task.aaaa} is in charge</p>
+        </div>
+      `;
+      },
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#gantt {
+}
+</style>
+
