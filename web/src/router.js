83dc2dcbd61d72cbf3837cfc1ecea3f79b22ff19
caussepa <caussepa@gmail.com>
2018-10-17 15:50:03 -0600

    fixed linting errors

--- web/src/router.js
+++ web/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Login from '@/views/Login.vue';
 
 Vue.use(Router);
 
@@ -19,8 +20,8 @@ export default new Router({
           path: '/projects',
           name: 'Projects',
           component: () => import(/* webpackChunkName: "projects" */ './views/Projects.vue'),
-        }
-      ]
-    }
+        },
+      ],
+    },
   ],
 });
