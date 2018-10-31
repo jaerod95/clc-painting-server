1b45bacf2daa034eb8c95f9630607dca58c18232
caussepa <caussepa@gmail.com>
2018-10-31 15:17:16 -0600

    wokring on Employee vue

--- web/src/views/Employees.vue
+++ web/src/views/Employees.vue
@@ -1,35 +1,45 @@
 <template>
- <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card elevation-4>
-                <v-flex pa-4>
-                    <v-card-img>
-                        <img src="@/assets/logo.png" alt="logo">
-                    </v-card-img>
-                    <v-card-text>
-                        <p id="employee_name">{{employee.name}}</p>
-                    </v-card-text>
+  <div id="app">
+    <v-app id="inspire">
+      <v-container fluid fill_height fill-width>
+        <v-layout column>
+          <v-flex xs12 sm6>
+            <v-toolbar color="indigo" dark>
+              <v-toolbar-title>ToolBar</v-toolbar-title>
+            </v-toolbar>
+            <v-container fluid grid-list-md>
+              <v-layout row wrap>
+                <v-flex
+                  v-for="employee in employees"
+                  v-bind="{ [`xs${employee.flex}`]: true }"
+                  :key="employee.name"
+                  >
+                  <v-card>
+                      <v-flex >
+                        <v-img :src="employee.src" height="30" width="30"></v-img>
+                      </v-flex>
+                    <v-flex>
+                      <p>{{employee.name}}</p>
+                    </v-flex>
+                  </v-card>
                 </v-flex>
-            </v-card>
+              </v-layout>
+            </v-container>
           </v-flex>
         </v-layout>
       </v-container>
-    </v-content>
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      employee: {
-        name: 'John Doe',
-      },
       employees: [
-        { employee: 'Foo' },
-        { employee: 'Bar' },
-        { employee: 'Bob' },
+        { name: 'Bob', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 2 },
+        { name: 'Tom', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 2 },
+        { name: 'David', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 2 },
       ],
     };
   },
