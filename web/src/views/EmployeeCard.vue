c22b35cc6aa10a2e25fea361dfd255d27339f90f
caussepa <caussepa@gmail.com>
2018-10-31 13:21:36 -0600

    added Employee Card vue

---
+++ web/src/views/EmployeeCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card elevation-4 class="loginCard">
+        <v-img
+            src="@/assets/logo.png">
+        </v-img>
+        <v-card-title primary title>
+            <p id="worker_name">{{name}} </p>
+        </v-card-title>
+    </v-card>
+</template>
+      
